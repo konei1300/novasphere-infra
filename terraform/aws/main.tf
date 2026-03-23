3633c8d557b92c7afa95870eca85588a2c8080f4
konei1300 <konei1300@gmail.com>
2026-03-23 17:08:11 +0100

    feat: ansible variables, boucles, templates Jinja2

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -47,6 +47,14 @@ resource "aws_security_group" "web" {
     cidr_blocks = [var.allowed_ssh_cidr]
   }
 
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
